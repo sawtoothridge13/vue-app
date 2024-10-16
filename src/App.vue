96e3a050be9f104d098e454564ce558cf3202104
Doug Andrews <sawtoothridge13@gmail.com>
2024-10-16 11:21:19 +0200

    Install vue.config

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <Header />
+  <Body />
+  <Footer />
+</template>
+
+<script>
+import Body from './components/body.vue';
+import Footer from './components/footer.vue';
+import Header from './components/header.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Body,
+    Footer,
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/body.vue
@@ -0,0 +1,14 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <img
+          src="./../assets/book-and-glasses.png"
+          alt="Book and glasses"
+          class="img-fluid mt-5"
+        />
+      </div>
+    </div>
+  </div>
+</template>
